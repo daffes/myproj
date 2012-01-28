e0236cfc092fde7f3b4bd93794be51c299622b88
Davi Costa <daffes@gmail.com>
2012-01-29 00:34:51 +0100

    Changed default example to work with 32 bits and wrote better instructions

--- example.py
+++ example.py
@@ -6,12 +6,15 @@
 #    break f
 #    run
 #    step until the end...
+# The two addresses are hardcoded based on a 32bits compilation
+# You may need to fix those addresses by those found
+# in the command "readelf -s simple" before stripping the file
 
 from elftools.elf.elffile import NormELFFile
 import sys
 
 f = NormELFFile(open(sys.argv[1]))
-f.symtab.add_symbol('f', 0x4004f4)
-f.symtab.add_symbol('main', 0x400504)
+f.symtab.add_symbol('f', 0x080483b4) 
+f.symtab.add_symbol('main', 0x080483c8)
 f.save(sys.argv[2])
 
