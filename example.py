866a4005401eb8555afb2e2870f6bdf88ca9d2be
Davi Costa <daffes@gmail.com>
2012-01-29 00:16:50 +0100

    Added useful header in example.py and refered in the README

--- example.py
+++ example.py
@@ -1,8 +1,17 @@
+# gcc simple.c -o simple
+# strip simple
+# python example.py simple out (can't change inplace for now)
+# gdb out
+#    break main
+#    break f
+#    run
+#    step until the end...
+
 from elftools.elf.elffile import NormELFFile
 import sys
 
 f = NormELFFile(open(sys.argv[1]))
 f.symtab.add_symbol('f', 0x4004f4)
 f.symtab.add_symbol('main', 0x400504)
-f.save('out')
+f.save(sys.argv[2])
 
